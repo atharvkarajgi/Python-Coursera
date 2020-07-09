65f6138f284f52e163777e01e1d62e6881af25ce
atharvkarajgi <atharvkarajgi@gmail.com>
2020-07-09 21:06:44 +0530

    First Commit

---
+++ Problem01.py
@@ -0,0 +1,2 @@
+print("Hello Everyone")
+print("This is my first python code")
---
+++ Problem02.py
@@ -0,0 +1,9 @@
+#program 02 from coursera.
+
+hrs = input("Enter hours: ")
+hours= int(hrs)
+rate = input("Enter rate: ")
+rate= float(rate)
+
+pay  = hours *rate
+Pay  =float(pay)
